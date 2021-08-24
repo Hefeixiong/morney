b637d82b2037a56ab08d4c832c77dd353698524d
hefeixiong <1074535830@qq.com>
2021-08-24 22:32:53 +0800

    创建Icon组件

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -2,23 +2,25 @@
   <div class="nav">
     <hr/>
     <router-link to="/money">
-      <svg><use xlink:href="#label"/></svg>
+      <Icon name="money"/>
       记账
     </router-link>
     |
-    <router-link to="/labels">标签</router-link>
+    <router-link to="/labels">
+      <Icon name="label"/>
+      标签</router-link>
     |
-    <router-link to="/statistics">统计</router-link>
+    <router-link to="/statistics">
+      <Icon name="statistic"/>
+      统计</router-link>
   </div>
 </template>
 
-<script>
-import x from '@/assets/icons/money.svg';
-import y from '@/assets/icons/label.svg'
+<script lang="ts">
 
 export default {
   name: 'Nav'
-}
+};
 </script>
 
 <style lang="scss" scoped>
